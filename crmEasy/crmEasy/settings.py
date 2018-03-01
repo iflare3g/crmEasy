aa27dc735ae9f23a015674d77af1c46b8c68e9c9
Mirco Grillo <mirco.grillomg@gmail.com>
2018-03-01 11:16:42 +0100

    Configures Postegres DB

--- crmEasy/crmEasy/settings.py
+++ crmEasy/crmEasy/settings.py
@@ -77,6 +77,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmEasy',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '/tmp',
+        'PORT': '5432',
     }
 }
 
